716d54396c152da46c1e88113c42508a70ff67f8
yerimchoi <mailharu365@gmail.com>
2024-03-01 23:41:28 +0900

    changes

---
+++ 자료구조+알고리즘/백준/단계별로 풀어보기/STEP10/1085.cpp
@@ -0,0 +1,12 @@
+#include <iostraem>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int x, y, w, h;
+    cin >> x >> y >> w >> h;
+    
+    
+    return 0;
+}
---
+++ 자료구조+알고리즘/백준/단계별로 풀어보기/STEP10/27323.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    cout << a * b;
+    return 0;
+}
